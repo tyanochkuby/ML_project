cec4ef6500d8da7284cfe5ad61d9905049ec5321
tyanochku_by <ramyud@st.amu.edu.pl>
2024-01-25 12:03:21 +0100

    all errors fixed input shape changed to 6419

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import data_prep
-# import ridge
-import keras
+import ridge_model
+import keras_model
 
 
 df = data_prep.get_data()
 X, y = df.drop(columns=[df.columns[0]], inplace=False), df.iloc[:, 0]
-# print('RIDGE:')
-# ridge.ridge(X, y)
-print('\n\nKERAS:')
-keras.keras(X, y)
+print('RIDGE:\n')
+ridge_model.main(X, y)
+print('\n\nKERAS:\n')
+keras_model.main(X, y)
